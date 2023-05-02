228ea577fb96ef0e66e846a4688b006975abeb04
Franco Pizzolante <Francogpizzolantesuarez@gmail.com>
2023-05-02 11:29:05 -0300

    Update n alturas.py

--- n alturas.py
+++ n alturas.py
@@ -1,7 +1,12 @@
 n=int(input("Ingrese cuántas alturas va a ingresar "))
-amax=0
+altura=int(input("Ingrese una altura"))
+amax=altura
+amin=altura
 for i in range (1,n+1):
     altura=int(input("Ingrese una altura"))
     if altura>amax:
         amax=altura
+    if altura<amin:
+        amin=altura
 print("La altura más alta es:",amax)
+print("La altura más baja es:",amin)
